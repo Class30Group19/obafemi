806a05fd86c3db23b6c5a7312ee4b0c418251743
Saheed Olufemi <femfem4uall@yahoo.com>
2022-11-25 23:58:19 +0000

    first commit

--- app.java
+++ app.java
@@ -1,5 +1,7 @@
 #This is code for the tesla project at hand
 #Please do not hard code
 #Kindly ensure all the standard narration are followed 
-#Please explain every steps 
-#author: Saheed Olufemi 
+#Please explain everything you do here 
+#author: Saheed Olufemi Aremu 
+#GitHub is a wonderful automation with high rate of intergrity
+#i like using secure code mgt like github  
